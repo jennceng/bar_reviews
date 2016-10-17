a017fdb1d73a81f2a5ac7df45aa02b2f2ca6b201
Jenn Eng <Jenn@Jennifers-MacBook-Pro-2.local>
2016-10-17 13:55:01 -0400

    get bar serializer to use review serializer

--- app/serializers/bar_serializer.rb
+++ app/serializers/bar_serializer.rb
@@ -1,7 +1,16 @@
 class BarSerializer < ActiveModel::Serializer
-  attributes :id, :name, :url, :average_rating, :reviews 
+  attributes :id,
+  :name,
+  :url,
+  :average_rating,
+  # ,:reviews
+
+  # the below uses the review serializer unlike line 6 which gives all the info
+  has_many :reviews
 
   def url
     url_for(object)
   end
 end
+
+# can be made via `rails g serializer bar` or manually create the file and inherit from ActiveModel::Serializer
